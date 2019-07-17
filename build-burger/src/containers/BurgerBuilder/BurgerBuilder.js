8125a74785191c62f96f5c034be8c39e09b37b3d
qubit88 <qubertec@gmail.com>
2019-07-17 21:18:55 +0300

    add 8.16

--- build-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ build-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  bacon: 1,
+  meat: 2
+};
 
 class BurgerBuilder extends Component {
   state = {
@@ -8,14 +16,30 @@ class BurgerBuilder extends Component {
       bacon: 1,
       cheese: 2,
       meat: 2
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </>
     );
   }
