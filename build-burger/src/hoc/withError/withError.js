80a52cd268f184e16a49c0f5d358dce52e84ece4
qubit88 <qubertec@gmail.com>
2019-07-18 20:38:45 +0300

    10.9

--- build-burger/src/hoc/withError/withError.js
+++ build-burger/src/hoc/withError/withError.js
@@ -10,14 +10,19 @@ const withError = (WrappedComponent, axios) => {
     // use constructor instead of componentWillMount for catching errors
 
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => this.setState({ error: error })
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
