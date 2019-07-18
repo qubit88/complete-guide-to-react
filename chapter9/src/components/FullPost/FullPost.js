d0729a376a8d8b11cb1ffee1a0f78c6f25d8c884
qubit88 <qubertec@gmail.com>
2019-07-18 17:41:33 +0300

    9.11

--- chapter9/src/components/FullPost/FullPost.js
+++ chapter9/src/components/FullPost/FullPost.js
@@ -21,6 +21,11 @@ class FullPost extends Component {
       }
     }
   }
+  deletePostHandler = () => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .then(res => console.log(res));
+  };
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
@@ -32,7 +37,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button onClick={this.deletePostHandler} className="Delete">
+              Delete
+            </button>
           </div>
         </div>
       );
