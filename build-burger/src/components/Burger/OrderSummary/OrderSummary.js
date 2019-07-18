21b0b0970f119f93e09801ce83df7b467113cd20
qubit88 <qubertec@gmail.com>
2019-07-18 14:33:29 +0300

    add shouldComponentupdate hook to prevent order summary from rendering when it's invisible

--- build-burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ build-burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(key => (
-    <li key={key}>
-      <span style={{ textTransform: "capitalize" }}>{key}</span>:
-      {props.ingredients[key]}
-    </li>
-  ));
-  return (
-    <>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total price</strong>: {props.total.toFixed(2)}
-      </p>
-      <p>Continue to checkout</p>
-      <Button clicked={props.purchaseCancelled} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={props.purchaseContinued} btnType="Success">
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("will update order summary");
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(key => (
+      <li key={key}>
+        <span style={{ textTransform: "capitalize" }}>{key}</span>:
+        {this.props.ingredients[key]}
+      </li>
+    ));
+
+    return (
+      <>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total price</strong>: {this.props.total.toFixed(2)}
+        </p>
+        <p>Continue to checkout</p>
+        <Button clicked={this.props.purchaseCancelled} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={this.props.purchaseContinued} btnType="Success">
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
 export default OrderSummary;
--- build-burger/src/components/UI/Modal/Modal.js
+++ build-burger/src/components/UI/Modal/Modal.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = props => {
-  return (
-    <>
-      <Backdrop clicked={props.modalClosed} show={props.show} />
-      <div
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0"
-        }}
-        className={classes.Modal}
-      >
-        {props.children}
-      </div>
-    </>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  render() {
+    return (
+      <>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+        <div
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
 
 export default Modal;
