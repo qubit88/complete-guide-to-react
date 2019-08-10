1b21329f974e9a5b58622960dae106a48d31fe41
qubit88 <qubertec@gmail.com>
2019-08-10 16:57:38 +0300

    pass data to component via search query

--- chapter12/src/containers/BurgerBuilder/BurgerBuilder.js
+++ chapter12/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -70,7 +70,21 @@ class BurgerBuilder extends Component {
     //   .then(res => this.setState({ loading: false, purchasing: false }))
     //   .catch(error => this.setState({ loading: false, purchasing: false }));
 
-    this.props.history.push("/checkout");
+    const queryParams = [];
+
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
+    });
   };
 
   addIngredientHandler = type => {
