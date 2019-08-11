ac235679640cac75dd463e86cfe199e57643e614
qubit88 <qubertec@gmail.com>
2019-08-11 20:29:44 +0300

    add fetching data for orders page

--- chapter12/src/containers/Orders/Orders.js
+++ chapter12/src/containers/Orders/Orders.js
@@ -1,16 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withError from "../../hoc/withError/withError";
 
 export class Orders extends Component {
-    render() {
-        return (
-            <div>
-                <Order></Order>
-                <Order></Order>
-            </div>
-        )
-    }
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios
+      .get("/orders")
+      .then(res => {
+        console.log(res.data);
+        const fetchedOrders = {};
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  }
 }
 
-export default Orders
+export default withError(Orders, axios);
